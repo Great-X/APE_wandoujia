b1665d5072f122b3c77a6e87298f93cbbca871e0
grx <18362916392@163.com>
2018-07-28 15:18:11 +0800

    update progress.

---
+++ reports/report-cn.ffcs.dh201405152200090337xxx001030.cartoonplayer/cn.ffcs.dh201405152200090337xxx001030.cartoonplayer-405/sataTimeline.vis.js
@@ -0,0 +1,17 @@
+var groups = new vis.DataSet();
+groups.add({id: 0, content: "SATA Action"});
+var items = new vis.DataSet([
+{start: new Date(1532750511814), title: "Sat Jul 28 00:01:51 EDT 2018@395", group: 0, screenURL: "step-395.png", id: 394, content: "BACK", rect: null},
+{start: new Date(1532750514171), title: "Sat Jul 28 00:01:54 EDT 2018@396", group: 0, screenURL: "step-396.png", id: 395, content: "BACK", rect: null},
+{start: new Date(1532750516462), title: "Sat Jul 28 00:01:56 EDT 2018@397", group: 0, screenURL: "step-397.png", id: 396, content: "SCROLL_RIGHT_LEFT", rect: [0,72,1080,1776]},
+{start: new Date(1532750517650), title: "Sat Jul 28 00:01:57 EDT 2018@398", group: 0, screenURL: "step-398.png", id: 397, content: "SCROLL_LEFT_RIGHT", rect: [0,72,1080,1776]},
+{start: new Date(1532750518507), title: "Sat Jul 28 00:01:58 EDT 2018@399", group: 0, screenURL: "step-399.png", id: 398, content: "SCROLL_RIGHT_LEFT", rect: [0,72,1080,1776]},
+{start: new Date(1532750519550), title: "Sat Jul 28 00:01:59 EDT 2018@400", group: 0, screenURL: "step-400.png", id: 399, content: "SCROLL_LEFT_RIGHT", rect: [0,72,1080,1776]},
+{start: new Date(1532750521504), title: "Sat Jul 28 00:02:01 EDT 2018@401", group: 0, screenURL: "step-401.png", id: 400, content: "SCROLL_RIGHT_LEFT", rect: [0,72,1080,1776]},
+{start: new Date(1532750522524), title: "Sat Jul 28 00:02:02 EDT 2018@402", group: 0, screenURL: "step-402.png", id: 401, content: "CLICK", rect: [1,216,1080,696]},
+{start: new Date(1532750523243), title: "Sat Jul 28 00:02:03 EDT 2018@403", group: 0, screenURL: "step-403.png", id: 402, content: "SCROLL_LEFT_RIGHT", rect: [0,72,1080,1776]},
+{start: new Date(1532750525197), title: "Sat Jul 28 00:02:05 EDT 2018@404", group: 0, screenURL: "step-404.png", id: 403, content: "SCROLL_RIGHT_LEFT", rect: [0,72,1080,1776]},
+{start: new Date(1532750526173), title: "Sat Jul 28 00:02:06 EDT 2018@405", group: 0, screenURL: "step-405.png", id: 404, content: "CLICK", rect: [32,1162,1048,1776]},
+]);
+var startTime = new Date(1532750511814);
+var endTime = new Date(1532750526173);
---
+++ reports/report-cn.ffcs.dh201405152200090337xxx001030.cartoonplayer/index.md
@@ -0,0 +1,8 @@
+title: cn.ffcs.dh201405152200090337xxx001030.cartoonplayer
+# cn.ffcs.dh201405152200090337xxx001030.cartoonplayer
+
+
+[Google Play Store](https://play.google.com/store/apps/details?id=cn.ffcs.dh201405152200090337xxx001030.cartoonplayer)
+
+
+* [cn.ffcs.dh201405152200090337xxx001030.cartoonplayer-405](./cn.ffcs.dh201405152200090337xxx001030.cartoonplayer-405/)
