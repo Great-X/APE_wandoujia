dca396d586d15b248dd823b28fb9d58736f97969
grx <18362916392@163.com>
2018-08-30 12:23:49 +0800

    update progress.

---
+++ reports/report-com.nineton.loveqzone3/com.nineton.loveqzone3-13/sataTimeline.vis.js
@@ -0,0 +1,17 @@
+var groups = new vis.DataSet();
+groups.add({id: 0, content: "SATA Action"});
+var items = new vis.DataSet([
+{start: new Date(1535265514407), title: "Sun Aug 26 02:38:34 EDT 2018@3", group: 0, screenURL: "step-3.png", id: 2, content: "CLICK", rect: [0,72,1080,612]},
+{start: new Date(1535265515134), title: "Sun Aug 26 02:38:35 EDT 2018@4", group: 0, screenURL: "step-4.png", id: 3, content: "CLICK", rect: [216,1632,432,1776]},
+{start: new Date(1535265518342), title: "Sun Aug 26 02:38:38 EDT 2018@5", group: 0, screenURL: "step-5.png", id: 4, content: "BACK", rect: null},
+{start: new Date(1535265520124), title: "Sun Aug 26 02:38:40 EDT 2018@6", group: 0, screenURL: "step-6.png", id: 5, content: "CLICK", rect: [438,1625,642,1693]},
+{start: new Date(1535265523388), title: "Sun Aug 26 02:38:43 EDT 2018@7", group: 0, screenURL: "step-7.png", id: 6, content: "CLICK", rect: [105,657,969,789]},
+{start: new Date(1535265524193), title: "Sun Aug 26 02:38:44 EDT 2018@8", group: 0, screenURL: "step-8.png", id: 7, content: "CLICK", rect: [765,1233,975,1275]},
+{start: new Date(1535265525560), title: "Sun Aug 26 02:38:45 EDT 2018@9", group: 0, screenURL: "step-9.png", id: 8, content: "SCROLL_BOTTOM_UP", rect: [0,72,1080,1572]},
+{start: new Date(1535265528823), title: "Sun Aug 26 02:38:48 EDT 2018@10", group: 0, screenURL: "step-10.png", id: 9, content: "CLICK", rect: [0,72,168,240]},
+{start: new Date(1535265529497), title: "Sun Aug 26 02:38:49 EDT 2018@11", group: 0, screenURL: "step-11.png", id: 10, content: "CLICK", rect: [864,1632,1080,1776]},
+{start: new Date(1535265530469), title: "Sun Aug 26 02:38:50 EDT 2018@12", group: 0, screenURL: "step-12.png", id: 11, content: "CLICK", rect: [648,1632,864,1776]},
+{start: new Date(1535265531241), title: "Sun Aug 26 02:38:51 EDT 2018@13", group: 0, screenURL: "step-13.png", id: 12, content: "CLICK", rect: [720,311,1080,710]},
+]);
+var startTime = new Date(1535265514407);
+var endTime = new Date(1535265531241);
---
+++ reports/report-com.nineton.loveqzone3/index.md
@@ -0,0 +1,8 @@
+title: com.nineton.loveqzone3
+# com.nineton.loveqzone3
+
+
+[Google Play Store](https://play.google.com/store/apps/details?id=com.nineton.loveqzone3)
+
+
+* [com.nineton.loveqzone3-13](./com.nineton.loveqzone3-13/)
